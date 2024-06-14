2109c1ac5c0490488ee9d411dca59cf1f48b4223
pon_stream24 <support@ponfes.com>
2024-06-14 23:54:22 +0900

    7.4.xに対応
    Co-authored-by: YukiTetsuka <153135690+YukiTetsuka@users.noreply.github.com>

--- src/utility.php
+++ src/utility.php
@@ -37,3 +37,16 @@ function loadPHPFileAll($_src){
         }
     }
 }
+
+function str_starts_with($haystack, $needle) {
+    $length = strlen($needle);
+    return (substr($haystack, 0, $length) === $needle);
+}
+
+function str_ends_with($haystack, $needle) {
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
+    return (substr($haystack, -$length) === $needle);
+}
